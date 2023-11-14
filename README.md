7e07a71cc40290ecc17adc5e16c94ce0482a315f
Lucas Kroth <lucas.kroth@inova.ind.br>
2023-11-14 14:24:05 -0300

    first commit v3.0

--- README.md
+++ README.md
@@ -4,63 +4,34 @@ Bibloteca que auxilia no controle dos motores da base STD (BS-80)
 
 ## Instalando
 
-Abra o terminal, e na pasta do script, execute:
+Abra o terminal, e na pasta raíz do script, execute:
 
-`npm i @libs-scripts-mep/std-control`
-
-Após finalizada a instalação da biblioteca, inclua em seu html:
-
-```html
-<script src="node_modules/@libs-scripts-mep/std-control/std-control.js"></script>
+```
+npm i @libs-scripts-mep/std-control
 ```
 
-> ⚠️Fique atento à ordem de carregamento dos arquivos, as dependências devem ser carregadas ANTES da biblioteca, como no trecho acima.
+## Desinstalando
 
-## Exemplo de Utilização
+Abra o terminal, e na pasta raíz do script, execute:
+
+```
+npm uninstall @libs-scripts-mep/std-control
+```
+
+## Atualizando
+
+Abra o terminal, e na pasta raíz do script, execute:
+
+```
+npm uninstall @libs-scripts-mep/std-control
+```
+
+## Como utilizar
+
+Realize a importação:
 
 ```js
-class TestScript {
-    constructor(eventMap, event) {
-
-        //instancia 
-        this.STD = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
-
-        //invoca roteiro de teste
-        this.Run()
-            .then(async () => {
-                //finaliza o teste
-            })
-            .catch(async (error) => {
-                this.STD.MoveUp(5000, false)
-                //finaliza o teste
-            })
-    }
-
-    async Run() {
-
-        //inicia observer da emergência, caso detectado acionamento, executa o then
-        this.EmergencyObserver().then((info) => { console.error(info.msg); alert(info.msg); throw null })
-
-        //verifica se motor está na posição inicial
-        const moveUpInit = await this.STD.MoveUp()
-        if (!moveUpInit.result) { this.RelatorioTeste.AddTesteFuncional("Sobe Motor [Inicio]", moveUpInit.msg, -1, false); throw null }
-
-        //Instrui operador para acionar bimanual [caminho de imagem ficticio]
-        await UI.setImage("./Imagens/biman.png")
-        await UI.setMsg("Pressione os botões indicados para iniciar o teste")
-
-        //Aguarda acionamento do bimanual (uso opcional, próximo método tem mesma lógica integrada)
-        const bimanMonitor = await this.BimanualObserver()
-        if (!bimanMonitor.result) { this.RelatorioTeste.AddTesteFuncional("Bimanual", bimanMonitor.msg, -1, false); throw null }
-
-        //aguarda descida do motor por 10s
-        const moveDown = await this.STD.MoveDown()
-        if (!moveDown.result) { this.RelatorioTeste.AddTesteFuncional("Desce Motor", moveDown.msg, -1, false); throw null }
-
-        //lógica de teste
-
-        //Manda motor subir sem aguardar todo o movimento, e avaça para finalização
-        const moveUpEnd = await this.STD.MoveUp()
-    }
-}
+import SmartTestDevice from "../node_modules/@libs-scripts-mep/std-control/std-control.js"
 ```
+
+As demais informações e instruções estarão disponíveis via `JSDocs`.
--- std-control.js
+++ std-control.js
@@ -1,20 +1,25 @@
-class SmartTestDevice {
-    /**
-     * 
-     * @param {Array} UpRelay array contendo o rele responsavel por fazer a base subir o motor
-     * @param {Array} DownRelay array contendo o rele responsavel por fazer a base descer o motor
-     * @param {string} TopLimitSwitch string informando a entrada relacionada ao fim de curso superior
-     * @param {string} BottomLimitSwitch string informando a entrada relacionada ao fim de curso inferior
-     * @param {string} Bimanual string informando a entrada relacionada ao Bimanual
-     * @param {string} Emergency string informando a entrada relacionada a Emergency
-     */
+/**
+ * # Exemplos
+ * 
+ * ```js
+ * this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+ * ```
+ */
+export default class SmartTestDevice {
     constructor(upRelay, downRelay, topLimitSwitch, bottomLimitSwitch, bimanual, emergency) {
+        /**Entrada do DAQ referente ao acionamento da microchave que detecta o limite `inferior` da movimentação do motor */
         this.BottomLimitSwitch = bottomLimitSwitch
+        /**Entrada do DAQ referente ao acionamento da microchave que detecta o limite `superior` da movimentação do motor */
         this.TopLimitSwitch = topLimitSwitch
+        /**Entrada do DAQ referente ao botão de `emergência`*/
         this.Emergency = emergency
+        /**Relé do DAQ referente ao comando de `descida` do motor*/
         this.DownRelay = downRelay
+        /**Relé do DAQ referente ao comando de `subida` do motor*/
         this.UpRelay = upRelay
+        /**Entrada do DAQ referente aos botões de comando `bimanual`*/
         this.Bimanual = bimanual
+        /**Flag que disponibiliza status de acionamento do botão de `emergência`*/
         this.EmergencyTriggered = false
     }
 
@@ -29,6 +34,25 @@ class SmartTestDevice {
     static EMERGENCY_TRIGGERED = "Emergência acionada!"
     static EMERGENCY_ALWAYS_TRIGGERED = "Emergência sempre acionada!"
 
+    /**
+     * 
+     * @param {Number} timeOut 
+     * @param {Boolean} expectedLevel 
+     * @returns Object
+     * 
+     * # Exemplos
+     * 
+     * ```js
+     * this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+     * const result = await this.Std.MoveDown()
+     * ```
+     * 
+     * ## Result
+     * 
+     * ```js
+     * {result: Boolean, msg: String}
+     * ```
+     */
     async MoveDown(timeOut = 5000, expectedLevel = false) {
 
         return new Promise((resolve) => {
@@ -38,33 +62,52 @@ class SmartTestDevice {
 
                     clearInterval(awaitsMoving)
                     clearTimeout(movingTimeOut)
-                    pvi.daq.desligaRele(this.UpRelay)
+                    DAQ.desligaRele(this.UpRelay)
                     resolve({ result: false, msg: SmartTestDevice.MOVING_INTERRUPT })
 
-                } else if (pvi.daq.in[this.BottomLimitSwitch].value == expectedLevel) {
+                } else if (DAQ.in[this.BottomLimitSwitch].value == expectedLevel) {
                     clearInterval(awaitsMoving)
                     clearTimeout(movingTimeOut)
-                    pvi.daq.desligaRele(this.DownRelay)
+                    DAQ.desligaRele(this.DownRelay)
                     resolve({ result: true, msg: SmartTestDevice.MOVING_SUCESS })
 
                 } else {
-                    if (pvi.daq.in[this.Bimanual].value == true) {
-                        pvi.daq.ligaRele(this.DownRelay)
+                    if (DAQ.in[this.Bimanual].value == true) {
+                        DAQ.ligaRele(this.DownRelay)
                     } else {
-                        pvi.daq.desligaRele(this.DownRelay)
+                        DAQ.desligaRele(this.DownRelay)
                     }
                 }
             }, 100)
 
             let movingTimeOut = setTimeout(() => {
                 clearInterval(awaitsMoving)
-                pvi.daq.desligaRele(this.DownRelay)
+                DAQ.desligaRele(this.DownRelay)
                 resolve({ result: false, msg: SmartTestDevice.MOVING_TIMEOUT })
             }, timeOut)
         })
 
     }
 
+    /**
+     * 
+     * @param {Number} timeOut 
+     * @param {Boolean} expectedLevel 
+     * @returns Object
+     * 
+     * # Exemplos
+     * 
+     * ```js
+     * this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+     * const result = await this.Std.MoveUp()
+     * ```
+     * 
+     * ## Result
+     * 
+     * ```js
+     * {result: Boolean, msg: String}
+     * ```
+     */
     async MoveUp(timeOut = 5000, expectedLevel = false) {
 
         return new Promise((resolve) => {
@@ -73,34 +116,63 @@ class SmartTestDevice {
 
                     clearInterval(awaitsMoving)
                     clearTimeout(movingTimeOut)
-                    pvi.daq.desligaRele(this.UpRelay)
+                    DAQ.desligaRele(this.UpRelay)
                     resolve({ result: false, msg: SmartTestDevice.MOVING_INTERRUPT })
 
-                } else if (pvi.daq.in[this.TopLimitSwitch].value == expectedLevel) {
+                } else if (DAQ.in[this.TopLimitSwitch].value == expectedLevel) {
 
                     clearInterval(awaitsMoving)
                     clearTimeout(movingTimeOut)
 
-                    pvi.daq.desligaRele(this.UpRelay)
+                    DAQ.desligaRele(this.UpRelay)
                     resolve({ result: true, msg: SmartTestDevice.MOVING_SUCESS })
                 } else {
-                    pvi.daq.ligaRele(this.UpRelay)
+                    DAQ.ligaRele(this.UpRelay)
                 }
             }, 100)
 
             let movingTimeOut = setTimeout(() => {
                 clearInterval(awaitsMoving)
-                pvi.daq.desligaRele(this.UpRelay)
+                DAQ.desligaRele(this.UpRelay)
                 resolve({ result: false, msg: SmartTestDevice.MOVING_TIMEOUT })
             }, timeOut)
         })
     }
 
+    /**
+     * 
+     * @param {String} input 
+     * @param {Number} timeOut 
+     * @param {Boolean} expectedLevel
+     * @returns Object
+     * 
+     * # Exemplos
+     * 
+     * ## Utilizando parâmetros default
+     * 
+     * ```js
+     * this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+     * const result = await this.Std.BimanualObserver()
+     * ```
+     * 
+     * ## Utilizando parâmetros personalizados
+     * 
+     * Passa 5000 para `timeout`, mantendo o valor default para `input` e `expecctedLevel`:
+     * ```js
+     * const result = await this.Std.BimanualObserver(undefined, 5000)
+     * ```
+     * 
+     * ## Result
+     * 
+     * ```js
+     * {result: Boolean, msg: String}
+     * ```
+     */
     async BimanualObserver(input = this.Bimanual, timeout = 15000, expectedLevel = true) {
         return new Promise((resolve) => {
-            if (pvi.daq.in[input].value == !expectedLevel) {
+            if (DAQ.in[input].value == !expectedLevel) {
 
-                pvi.daq.in[input].onChange = (logicLevel) => {
+                DAQ.in[input].onChange = (logicLevel) => {
                     if (logicLevel == expectedLevel) {
                         clearTimeout(timeOutMonitor)
                         resolve({ result: true, msg: SmartTestDevice.BIMANUAL_TRIGGERED })
@@ -111,16 +183,36 @@ class SmartTestDevice {
             }
 
             const timeOutMonitor = setTimeout(() => {
-                pvi.daq.in[input].onChange = () => { }
+                DAQ.in[input].onChange = () => { }
                 resolve({ result: false, msg: SmartTestDevice.BIMANUAL_TIMEOUT })
             }, timeout)
         })
     }
 
+    /**
+     * 
+     * @param {Number} input 
+     * @returns Object
+     * 
+     * # Exemplos
+     * 
+     * Utilizando parâmetros default:
+     * 
+     * ```js
+     * this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+     * const result = await this.Std.EmergencyObserver()
+     * ```
+     * 
+     * ## Result
+     * 
+     * ```js
+     * {result: Boolean, msg: String}
+     * ```
+     */
     async EmergencyObserver(input = this.Emergency) {
         return new Promise((resolve) => {
-            if (pvi.daq.in[input].value == true) {
-                pvi.daq.in[input].onChange = (logicLevel) => {
+            if (DAQ.in[input].value == true) {
+                DAQ.in[input].onChange = (logicLevel) => {
                     if (logicLevel == false) {
                         this.EmergencyTriggered = true
                         resolve({ triggered: true, msg: SmartTestDevice.EMERGENCY_TRIGGERED })
